d3e00895480bdea77132faa7df1c3f16d7f1eda4
LudoPilot <e.berardfr@gmail.com>
2024-05-15 00:49:05 +0200

    Ajout d'un menu pause activable par la touche échap

---
+++ Assets/Scripts/MenuPause.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public GameObject gameOverMenu;
+
+    /*
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }*/
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameOverMenu.activeSelf)
+            {
+                return;
+            }
+
+            if (Time.timeScale == 1)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu"); // vérifier le nom avec Paul
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Sortie du jeu");
+        Application.Quit();
+    }
+}
