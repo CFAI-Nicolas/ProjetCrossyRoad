d5d51bbb560ea42d18ba629ab989e75b7597e381
Paul TOUSSAINT <paul-toussaint@laposte.net>
2024-05-15 12:12:30 +0200

    refonte du menu

---
+++ Assets/ChangeSceneOnClick.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnClick : MonoBehaviour
+{
+    // Le nom de la scène vers laquelle changer
+    [SerializeField] private string Demo1;
+
+    void Update()
+    {
+        // Vérifie si un clic de souris est détecté
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Charge la scène spécifiée
+            SceneManager.LoadScene(Demo1);
+        }
+    }
+}
+
